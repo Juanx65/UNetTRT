40b9ef3564c08fc76e76f2fa16131073845a94e8
Juanx65 <juan.aguileraca@sansano.usm.cl>
2023-09-05 00:15:42 -0300

    se parametrisa eval y train

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from utils.functions import destandarize
 from utils.load_data import MyDataLoader
 from matplotlib.ticker import MaxNLocator
+import argparse
 
 train_on_gpu = torch.cuda.is_available()
 
@@ -17,120 +18,161 @@ else:
 
 device = torch.device("cuda:0" if train_on_gpu else "cpu")
 
-# Hyperparameters
-num_filters = 29
-val_dropout = 0.0
-learning_rate = 0.001
-batch_size = 32
+def eval(opt):
+    # LOAD DATA
+    my_data_loader = MyDataLoader()
+    x_test, y_test, y_mean, y_std, fs_test, r_test, z_test= my_data_loader.load_test_data()
 
-# LOAD DATA
-my_data_loader = MyDataLoader()
-#x_test, y_test, y_mean, y_std, fs_test, r_test, z_test= my_data_loader.load_test_data()
-_, _, y_mean, y_std, _, _, _= my_data_loader.load_test_data()
-x_test, t_emi = my_data_loader.load_data_exp()
+    x_test = torch.tensor(x_test).float().to(device)
+    y_test = torch.tensor(y_test).float().to(device)
 
-x_test = torch.tensor(x_test).float().to(device)
-#y_test = torch.tensor(y_test).float().to(device)
+    ####LOAD######
+    model = U_Net(n1=opt.num_filters, dropout_rate=opt.dropout)
+    model = torch.load(opt.weights)
+    model.to(device)
+    model.eval()
+
+    # Eval 
+    with torch.no_grad():
+            output = model(x_test)
+
+    x_test = x_test.cpu()
+    y_test = y_test.cpu()
+
+    y_test_pred = destandarize(output, y_mean, y_std)
+    y_test_pred = y_test_pred.squeeze(1)
+
+    y_test_pred = y_test_pred.cpu()
+
+    #N = random.randint(0,len(x_test)-1)
+    N = 11
+
+    y_test[N][fs_test[N] <= 0.05e-6] = 0
+    y_test_pred[N][fs_test[N] <= 0.05e-6] = 0
+    print(N)
+
+    plt.rcParams['figure.figsize'] = [12, 4]
+
+    plt.subplot(171)
+    plt.imshow(x_test[0,0,:,:], cmap = 'jet')#, vmax=x_test.max() , vmin=x_test.min())
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
+
+    plt.subplot(172)
+    plt.imshow(x_test[0,1,:,:], cmap = 'jet')#, vmax=x_test.max(), vmin=x_test.min())
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
+
+    plt.subplot(173)
+    plt.imshow(x_test[0,2,:,:], cmap = 'jet')#, vmax=x_test.max(), vmin=x_test.min())
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
+
+    plt.subplot(174)
+    plt.imshow(y_test[N,:,:], cmap = 'jet', vmin=1500, vmax=2205)
+    plt.title('$Groundtruth$')
+    plt.colorbar()
+
+    plt.subplot(175)
+    plt.imshow(y_test_pred[N], cmap = 'jet', vmin=1500, vmax=2205)
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
+
+    abs_err = np.abs(y_test[N] - y_test_pred[N])
+    plt.subplot(176)
+    plt.imshow(abs_err, cmap = 'jet', vmin=0, vmax=50)
+    plt.title('$\Delta T_{s}$')
+    plt.colorbar(ticks=MaxNLocator(6))
+
+    plt.show()
+
+    print('Abs. error max:', abs_err.max())
+    print('Abs. error min:', abs_err.min())
+    print('Abs. error mean:', abs_err.mean())
+    print('Abs. error stddev:', abs_err.std())
+    print('Abs. error %:', abs_err.mean()*100/y_test[N].mean())
+
+def eval_exp(opt):
+    print("experiment")
+    # LOAD DATA
+    my_data_loader = MyDataLoader()
+    _, _, y_mean, y_std, _, _, _= my_data_loader.load_test_data()
+    x_test, t_emi = my_data_loader.load_data_exp()
+
+    x_test = torch.tensor(x_test).float().to(device)
 
 
-####LOAD######
-model = U_Net(n1=num_filters, dropout_rate=val_dropout)
-model = torch.load('weights/best.pth')
-model.to(device)
-model.eval()
+    ####LOAD######
+    model = U_Net(n1=opt.num_filters, dropout_rate=opt.dropout)
+    model = torch.load(opt.weights)
+    model.to(device)
+    model.eval()
 
-# Eval 
-with torch.no_grad():
-        output = model(x_test)
+    # Eval 
+    with torch.no_grad():
+            output = model(x_test)
 
-x_test = x_test.cpu()
-#y_test = y_test.cpu()
+    x_test = x_test.cpu()
 
-y_test_pred = destandarize(output, y_mean, y_std)
-y_test_pred = y_test_pred.squeeze(1)
+    y_test_pred = destandarize(output, y_mean, y_std)
+    y_test_pred = y_test_pred.squeeze(1)
 
-y_test_pred = y_test_pred.cpu()
+    y_test_pred = y_test_pred.cpu()
 
-print("x_test size", x_test.size())
+    t_emi = t_emi[::-1]
+    mask = t_emi<1
+    y_test_pred = np.ma.masked_where(mask, y_test_pred[0])
 
-#N = random.randint(0,len(x_test)-1)
-N = 11
+    plt.rcParams['figure.figsize'] = [12, 4]
+    plt.subplot(141)
+    plt.imshow(x_test[0,0,:,:], cmap = 'jet', vmax=x_test.max() , vmin=x_test.min())
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
 
-""" y_test[N][fs_test[N] <= 0.05e-6] = 0
-y_test_pred[N][fs_test[N] <= 0.05e-6] = 0
-print(N)
+    plt.subplot(142)
+    plt.imshow(x_test[0,1,:,:], cmap = 'jet', vmax=x_test.max(), vmin=x_test.min())
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
 
-plt.rcParams['figure.figsize'] = [12, 4]
+    plt.subplot(143)
+    plt.imshow(x_test[0,2,:,:], cmap = 'jet', vmax=x_test.max(), vmin=x_test.min())
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
 
-plt.subplot(171)
-plt.imshow(x_test[0,0,:,:], cmap = 'jet')#, vmax=x_test.max() , vmin=x_test.min())
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
+    plt.subplot(144)
+    plt.imshow(y_test_pred, cmap = 'jet', vmin=1500, vmax=2205)
+    plt.title('$U-Net$')
+    plt.colorbar()
+    plt.tight_layout()
 
-plt.subplot(172)
-plt.imshow(x_test[0,1,:,:], cmap = 'jet')#, vmax=x_test.max(), vmin=x_test.min())
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
+    plt.show()
 
-plt.subplot(173)
-plt.imshow(x_test[0,2,:,:], cmap = 'jet')#, vmax=x_test.max(), vmin=x_test.min())
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', default = 32, type=int,help='batch size to train')
+    parser.add_argument('--epochs', default = 100, type=int,help='epoch to train')
+    parser.add_argument('--dropout', default = 0.089735, type=float,help='percentage dropout to use')
+    parser.add_argument('--num_filters', default = 29, type=int,help='Canales de salida de la primera capa conv')
+    parser.add_argument('--learning_rate', default = 0.000410, type=float, help='learning rate')
+    parser.add_argument('--weights', default= 'weights/best.pth', type=str, help='path to weights')
+    parser.add_argument('--experiment', action='store_true', help='si es experimento ')
+    opt = parser.parse_args()
+    return opt
 
-plt.subplot(174)
-plt.imshow(y_test[N,:,:], cmap = 'jet', vmin=1500, vmax=2205)
-plt.title('$Groundtruth$')
-plt.colorbar()
+def main(opt):
 
-plt.subplot(175)
-plt.imshow(y_test_pred[N], cmap = 'jet', vmin=1500, vmax=2205)
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
+    if(opt.experiment):
+        eval_exp(opt)
+    else:
+        eval(opt)
 
-abs_err = np.abs(y_test[N] - y_test_pred[N])
-plt.subplot(176)
-plt.imshow(abs_err, cmap = 'jet', vmin=0, vmax=50)
-plt.title('$\Delta T_{s}$')
-plt.colorbar(ticks=MaxNLocator(6)) """
-
-
-t_emi = t_emi[::-1]
-mask = t_emi<1
-y_test_pred = np.ma.masked_where(mask, y_test_pred[0])
-
-plt.rcParams['figure.figsize'] = [12, 4]
-plt.subplot(141)
-plt.imshow(x_test[0,0,:,:], cmap = 'jet', vmax=x_test.max() , vmin=x_test.min())
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
-
-plt.subplot(142)
-plt.imshow(x_test[0,1,:,:], cmap = 'jet', vmax=x_test.max(), vmin=x_test.min())
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
-
-plt.subplot(143)
-plt.imshow(x_test[0,2,:,:], cmap = 'jet', vmax=x_test.max(), vmin=x_test.min())
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
-
-plt.subplot(144)
-plt.imshow(y_test_pred, cmap = 'jet', vmin=1500, vmax=2205)
-plt.title('$U-Net$')
-plt.colorbar()
-plt.tight_layout()
-
-plt.show()
-
-""" print('Abs. error max:', abs_err.max())
-print('Abs. error min:', abs_err.min())
-print('Abs. error mean:', abs_err.mean())
-print('Abs. error stddev:', abs_err.std())
-print('Abs. error %:', abs_err.mean()*100/y_test[N].mean()) """
+if __name__ == '__main__':
+    opt = parse_opt()
+    main(opt)
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from models.unet import U_Net
 from utils.load_data import MyDataLoader
 from torch.utils.data import TensorDataset, DataLoader
 from utils.functions import mae_percentage
+import argparse
 
 
 train_on_gpu = torch.cuda.is_available()
@@ -18,78 +19,84 @@ else:
 
 device = torch.device("cuda:0" if train_on_gpu else "cpu")
 
-# Hyperparameters
-num_filters = 29
-val_dropout = 0.0
-learning_rate = 0.001
-batch_size = 32
+def train(opt):
+    # Datos de entrenamiento
+    my_data_loader = MyDataLoader()
+    x_train, x_valid, y_train, y_valid, y_mean, y_std = my_data_loader.load_data()
 
-# Datos de entrenamiento
-my_data_loader = MyDataLoader()
-x_train, x_valid, y_train, y_valid, y_mean, y_std = my_data_loader.load_data()
+    x_train = torch.tensor(x_train).float()
+    y_train = torch.tensor(y_train).unsqueeze(1).float()
+    x_valid = torch.tensor(x_valid).float()
+    y_valid = torch.tensor(y_valid).unsqueeze(1).float()
 
-x_train = torch.tensor(x_train).float()
-y_train = torch.tensor(y_train).unsqueeze(1).float()
-x_valid = torch.tensor(x_valid).float()
-y_valid = torch.tensor(y_valid).unsqueeze(1).float()
+    tensor_dataset = TensorDataset(x_train, y_train)
+    train_loader = DataLoader(tensor_dataset, batch_size=opt.batch_size, shuffle=True)
 
-tensor_dataset = TensorDataset(x_train, y_train)
-train_loader = DataLoader(tensor_dataset, batch_size=batch_size, shuffle=True)
+    tensor_dataset = TensorDataset(x_valid, y_valid)
+    valid_loader = DataLoader(tensor_dataset, batch_size=opt.batch_size, shuffle=False)
 
-tensor_dataset = TensorDataset(x_valid, y_valid)
-valid_loader = DataLoader(tensor_dataset, batch_size=batch_size, shuffle=False)
+    # Cargar modelo
+    model = U_Net(n1=opt.num_filters, dropout_rate=opt.dropout)
+    model.to(device)
+    torchsummary.summary(model, input_size=(3, 128, 32))
+    loss_function = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=opt.learning_rate)
 
-# Cargar modelo
-model = U_Net(n1=num_filters, dropout_rate=val_dropout)
-model.to(device)
-torchsummary.summary(model, input_size=(3, 88, 32))
-loss_function = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # Entrenamiento
+    valid_loss_min = np.Inf
+    n_epoch = opt.epochs
+    for epoch in range(n_epoch):
+        train_loss = 0.0
+        valid_loss = 0.0
+        for batch_idx, (x_batch, y_batch) in enumerate(train_loader):
 
-# Entrenamiento
-valid_loss_min = np.Inf
-n_epoch = 100
-for epoch in range(n_epoch):
-    train_loss = 0.0
-    valid_loss = 0.0
-    for batch_idx, (x_batch, y_batch) in enumerate(train_loader):
+            x_batch = x_batch.to(device)
+            y_batch = y_batch.to(device)
 
-        x_batch = x_batch.to(device)
-        y_batch = y_batch.to(device)
+            y_pred = model(x_batch) # Forward
+            loss = loss_function(y_pred, y_batch) # Cálculo de la pérdida
 
-        y_pred = model(x_batch) # Forward
-        loss = loss_function(y_pred, y_batch) # Cálculo de la pérdida
+            # Backward y optimización
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        # Backward y optimización
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            train_loss += loss.item()*x_batch.size(0)
+        
+        for batch_idx, (x_batch, y_batch) in enumerate(valid_loader):
 
-        train_loss += loss.item()*x_batch.size(0)
-    
-    for batch_idx, (x_batch, y_batch) in enumerate(valid_loader):
+            x_batch = x_batch.to(device)
+            y_batch = y_batch.to(device)
 
-        x_batch = x_batch.to(device)
-        y_batch = y_batch.to(device)
+            y_pred = model(x_batch)
+            loss = mae_percentage(y_pred, y_batch, y_mean, y_std)
 
-        y_pred = model(x_batch)
-        loss = mae_percentage(y_pred, y_batch, y_mean, y_std)
+            valid_loss +=  loss.item()*x_batch.size(0)
 
-        valid_loss +=  loss.item()*x_batch.size(0)
+        if( valid_loss < valid_loss_min):
+            print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model '.format(valid_loss_min, valid_loss))
+            torch.save(model, 'weights/best.pth')
+            valid_loss_min = valid_loss
+        
+        train_loss = train_loss / len(x_train)
+        valid_loss = valid_loss / len(x_valid)
+        if (epoch+1) % 1 == 0:
+            print('Epoch: {}/{} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch + 1, n_epoch, train_loss,valid_loss))
 
-    if( valid_loss < valid_loss_min):
-        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model '.format(valid_loss_min, valid_loss))
-        torch.save(model, 'weights/best.pth')
-        valid_loss_min = valid_loss
-    
-    train_loss = train_loss / len(x_train)
-    valid_loss = valid_loss / len(x_valid)
-    if (epoch+1) % 1 == 0:
-        print('Epoch: {}/{} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch + 1, n_epoch, train_loss,valid_loss))
 
-"""
-# Prueba del modelo entrenado
-with torch.no_grad():
-    test_data = torch.tensor([[5.0]])
-    predicted = model(test_data)
-    print(f'Predicción después de entrenamiento: f(5) = {predicted.item()}') """
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', default = 32, type=int,help='batch size to train')
+    parser.add_argument('--epochs', default = 100, type=int,help='epoch to train')
+    parser.add_argument('--dropout', default = 0.089735, type=float,help='percentage dropout to use')
+    parser.add_argument('--num_filters', default = 29, type=int,help='Canales de salida de la primera capa conv')
+    parser.add_argument('--learning_rate', default = 0.000410, type=float, help='learning rate')
+    opt = parser.parse_args()
+    return opt
+
+def main(opt):
+    train(opt)
+
+if __name__ == '__main__':
+    opt = parse_opt()
+    main(opt)
