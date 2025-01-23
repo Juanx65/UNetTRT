b79571b3eecf8aa5c7f51cf09bb78283b0e3dc9b
jorgeaportilla <jorge.portilla@sansano.usm.cl>
2025-01-23 15:52:59 -0300

    add kernel size

--- utils/models/unet.py
+++ utils/models/unet.py
@@ -6,15 +6,15 @@ import torch.nn.functional as F
 
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_ch, out_ch, mid_ch=None):
+    def __init__(self, in_ch, out_ch, kernelsize = 3, mid_ch=None):
         super(DoubleConv, self).__init__()
         if not mid_ch:
             mid_ch = out_ch
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_ch, mid_ch, kernel_size=3, stride = 1, padding=1, bias=True),
+            nn.Conv2d(in_ch, mid_ch, kernel_size=kernelsize, stride = 1, padding=1, bias=True),
             nn.BatchNorm2d(mid_ch, momentum=0.99 ),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_ch, out_ch, kernel_size=3, stride= 1, padding=1, bias=True),
+            nn.Conv2d(mid_ch, out_ch, kernel_size=kernelsize, stride= 1, padding=1, bias=True),
             nn.BatchNorm2d(out_ch, momentum=0.99 ),
             nn.ReLU(inplace=True)
         )
@@ -24,24 +24,24 @@ class DoubleConv(nn.Module):
         return x
 
 class conv_block(nn.Module):
-    def __init__(self, in_ch, out_ch,dropout_rate):
+    def __init__(self, in_ch, out_ch, kernelsize,dropout_rate):
         super(conv_block, self).__init__()
         
         self.conv = nn.Sequential(
-            DoubleConv(in_ch, out_ch),
+            DoubleConv(in_ch, out_ch, kernelsize),
             nn.Dropout2d(p=dropout_rate)
         )
 
     def forward(self, x):
         x = self.conv(x)
         return x
-    
+
 class down_block(nn.Module):
-    def __init__(self, in_ch, out_ch,dropout_rate):
+    def __init__(self, in_ch, out_ch, dropout_rate, kernelsize):
         super(down_block, self).__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_ch, out_ch),
+            DoubleConv(in_ch, out_ch, kernelsize),
             nn.Dropout2d(p=dropout_rate)
         )
 
@@ -49,10 +49,10 @@ class down_block(nn.Module):
         return self.maxpool_conv(x)
 
 class up_block(nn.Module):
-    def __init__(self, in_ch, out_ch,dropout_rate):
+    def __init__(self, in_ch, out_ch,dropout_rate, kernelsize):
         super(up_block, self).__init__()
         self.up = nn.Sequential(
-            nn.ConvTranspose2d(in_ch, in_ch // 2, kernel_size=3, stride=2),
+            nn.ConvTranspose2d(in_ch, in_ch // 2, kernel_size=kernelsize, stride=2),
             nn.BatchNorm2d(in_ch // 2, momentum=0.99 )
         )
         self.conv = nn.Sequential(
@@ -73,20 +73,20 @@ class up_block(nn.Module):
 
 class U_Net(nn.Module):
  
-    def __init__(self, in_ch=3, out_ch=1, n1=29, dropout_rate=0.089735):
+    def __init__(self, in_ch=3, out_ch=1, n1=29, kernelsize = 3, dropout_rate=0.089735):
         super(U_Net, self).__init__()
 
         filters = [n1, n1 * 2, n1 * 4, n1 * 8]
 
-        self.In = conv_block(in_ch, filters[0],dropout_rate)
+        self.In = conv_block(in_ch, filters[0], kernelsize, dropout_rate)
 
-        self.Down1 = down_block(filters[0], filters[1],dropout_rate)
-        self.Down2 = down_block(filters[1], filters[2],dropout_rate)
-        self.Down3 = down_block(filters[2], filters[3],dropout_rate)
+        self.Down1 = down_block(filters[0], filters[1], dropout_rate, kernelsize)
+        self.Down2 = down_block(filters[1], filters[2], dropout_rate, kernelsize)
+        self.Down3 = down_block(filters[2], filters[3], dropout_rate, kernelsize)
 
-        self.Up3 = up_block(filters[3], filters[2],dropout_rate)
-        self.Up2 = up_block(filters[2], filters[1],dropout_rate)
-        self.Up1 = up_block(filters[1], filters[0],dropout_rate)
+        self.Up3 = up_block(filters[3], filters[2], dropout_rate, kernelsize)
+        self.Up2 = up_block(filters[2], filters[1], dropout_rate, kernelsize)
+        self.Up1 = up_block(filters[1], filters[0], dropout_rate, kernelsize)
 
         self.Out = nn.Conv2d(filters[0], out_ch, kernel_size=1, stride=1, padding=0)
 
