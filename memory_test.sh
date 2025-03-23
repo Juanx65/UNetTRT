419dae67d8896734c171d9004995c77e271692b2
juanx65 <juan.aguileraca@sansano.usm.cl>
2025-03-22 22:33:11 -0300

    update name for sh scripts to test memory and latency

--- memory_test.sh
+++ memory_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define los modelos y los tamaños de batch size
-model_typr="attunet"
-model="attunet.pth"
+model_typr="unet"
+model="unet.pth"
 batch_size=32
-log_name="attunet_b32_oplvl3_pm2_hot"
+log_name="unet_base_pm0_cold"
 
 tegrastats --interval 1 --logfile outputs/tegrastats_log/${log_name}.txt & #sudo tegrastats si necesitas ver mas metricas
 tegrastat_pid=$!
-python eval.py --weights weights/$model --model $model_typr --dataset datasets/img_preprocess --latency --batch_size $batch_size
+python eval.py --weights weights/$model --model $model_typr --dataset datasets/conjunto_experimental/condA --latency --batch_size $batch_size > ${log_name}.txt
 kill -9 $tegrastat_pid
